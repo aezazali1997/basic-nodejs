94e34bbdeddf411215cc294c6af7844ff219f821
aezazali1997 <aezazali.pro@gmail.com>
2023-07-20 10:41:56 +0500

    commit

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 8001;
 const app = express();
+app.use('/user', (req, res) => {
+    res.status(200).send('just testing if this routes is working or not')
+})
 app.use('/', (req, res) => {
-    console.log('app is working greate')
+    console.log('app is working great')
     res.status(200).send('hello')
 })
+
 
 app.listen(PORT, () => {
     console.log(`app is listening on port ${PORT}`)
